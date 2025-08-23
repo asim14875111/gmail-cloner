020478626d274ea8ccc9dae105b1cc98907271f7
asim14875111 <asim260791@gmail.com>
2025-08-23 18:10:30 +0500

    Task updated

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBiXlX61xxMFqvZ9vHGDsmhbPghrR9uoJw",
-  authDomain: "nextauthapp-4d566.firebaseapp.com",
-  projectId: "nextauthapp-4d566",
-  storageBucket: "nextauthapp-4d566.firebasestorage.app",
-  messagingSenderId: "202555445723",
-  appId: "1:202555445723:web:0a5ab62df0bf3d7ddd57fe",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
